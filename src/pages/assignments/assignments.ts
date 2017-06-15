bec929d549e4cce305ef9315eecd0cdfd12c5881
eaailkal <eaailkal@students.eaaa.dk>
2017-06-15 15:29:27 +0200

    doughnut chart for assignments overview

--- src/pages/assignments/assignments.ts
+++ src/pages/assignments/assignments.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild  } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Chart } from 'chart.js';
 
 
 @IonicPage()
@@ -9,11 +10,37 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AssignmentsPage {
 
+  @ViewChild('doughnutCanvas') doughnutCanvas;
+
+  doughnutChart: any;
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AssignmentsPage');
+
+    this.doughnutChart = new Chart(this.doughnutCanvas.nativeElement, {
+ 
+            type: 'doughnut',
+            data: {
+                labels: ["Awaiting", "Completed"],
+                datasets: [{
+                    label: 'Assignments overview',
+                    data: [3, 2],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(117, 179, 192, 0.2)'
+                    ],
+                    hoverBackgroundColor: [
+                        "#FF6384",
+                        "#75b3c0"
+                    ]
+                }]
+            }
+ 
+        });
+
   }
 
 }
